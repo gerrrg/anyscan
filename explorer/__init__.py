f282619097e695e0dd044705fcfdb051559feaa9
orthogonalglobal <45737169+orthogonalglobal@users.noreply.github.com>
2022-01-18 14:37:56 -0500

    Generalize etherscan for all etherscan like apis

--- explorer/__init__.py
+++ explorer/__init__.py
@@ -1,4 +1,4 @@
-from .etherscan import Etherscan
+from .explorer import BlockchainExplorer
 from .modules.accounts import Accounts as accounts
 from .modules.blocks import Blocks as blocks
 from .modules.contracts import Contracts as contracts
